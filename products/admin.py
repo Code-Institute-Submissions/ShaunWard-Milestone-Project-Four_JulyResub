3b1b1c2f37a53188e6d0362595f9cd2249e02ee9
ShaunWard <shaunalwynward@googlemail.com>
2021-04-13 20:29:10 +0000

    Updated requirements.txt, updated models.py

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 from .models import Product, Category
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'description',
+        'dimensions',
+        'image',
+        'is_sold',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
 admin.site.register(Product)
 admin.site.register(Category)
--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
 
     def __str__(self):
@@ -9,12 +13,12 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     dimensions = models.CharField(max_length=70, null=True, blank=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     image = models.ImageField(null=True, blank=True)
     is_sold = models.BooleanField(default=False)
 
