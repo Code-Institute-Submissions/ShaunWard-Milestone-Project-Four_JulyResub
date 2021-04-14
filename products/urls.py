4ecf5709a667f3c5a881eae96f2244d1a7483777
ShaunWard <shaunalwynward@googlemail.com>
2021-04-14 17:29:46 +0000

    Built basic product detail to check url, view and template

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'), # route url that will render all_products from views.py
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
 ]
