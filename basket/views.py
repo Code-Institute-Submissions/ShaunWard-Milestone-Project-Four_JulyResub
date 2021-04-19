815b043ceb33470b273d41a1ef108a047f42ca1c
ShaunWard <shaunalwynward@googlemail.com>
2021-04-19 20:51:39 +0000

    Added add_to_basket view to add product_id to session to keep track of users basket items

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+import sweetify
 
 
 def view_basket(request):
@@ -7,3 +9,20 @@ def view_basket(request):
     return render(request, 'basket/basket.html')
 
 
+def add_to_basket(request, item_id):
+    '''Adds the item to the basket'''
+
+    quantity = 1
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id not in basket:
+        basket[item_id] = quantity
+        sweetify.sweetalert(request, 'Image added to your basket', timer=1000)
+    elif item_id in basket:
+        sweetify.sweetalert(request, 'Image already in your basket', timer=1000)
+    else:
+        sweetify.sweetalert(request, 'Please try again later', text='Image could not be added at this time', persistent='Ok')
+
+    request.session['basket'] = basket
+    return redirect(redirect_url)
