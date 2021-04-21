51484d58e86e6756675825d7beebe651d6472003
ShaunWard <shaunalwynward@googlemail.com>
2021-04-21 22:21:16 +0000

    Using contexts.py to display basket info in basket.html template

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def basket_contents(request):
@@ -6,6 +8,15 @@ def basket_contents(request):
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += product.price
+        basket_items.append({
+            'item_id': item_id,
+            'product': product,
+        })
 
     context = {
         'basket_items': basket_items,
