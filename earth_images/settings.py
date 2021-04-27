28cf5b56f372ce3ed6dfbc943f6a958db752075a
ShaunWard <shaunalwynward@googlemail.com>
2021-04-27 22:28:01 +0000

    Removed secret key and set debug

--- earth_images/settings.py
+++ earth_images/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ayh79@7rf&7@#i*y@dv)@2c^!(gx@sz*3j#izgh7ay$y0j81aw'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['milestone-project-four-ecom.herokuapp.com', 'localhost']
 
