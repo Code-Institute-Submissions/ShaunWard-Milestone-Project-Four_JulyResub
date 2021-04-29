4f7ea20a8739b18bb8184979af8366cff64d14b2
ShaunWard <shaunalwynward@googlemail.com>
2021-04-29 19:05:30 +0000

    Commented out code to check if this made build fail on heroku

--- community/views.py
+++ community/views.py
@@ -23,26 +23,26 @@ def all_community_posts(request):
     return render(request, template, context)
 
 
-def add_post(request):
-    # """ post to the community page """
-    # if not request.user.is_authenticated:
-    #     sweetify.sweetalert(request, 'Please sign in to post on the form', persistent='Ok')
-    #     return redirect(reverse('home'))
+# def add_post(request):
+#     """ post to the community page """
+#     if not request.user.is_authenticated:
+#         sweetify.sweetalert(request, 'Please sign in to post on the form', persistent='Ok')
+#         return redirect(reverse('home'))
 
-    # if request.method == 'POST':
-    #     form = PostForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         post = form.save()
-    #         sweetify.sweetalert(request, 'Success! Product Added', timer=1000)
-    #         return redirect(reverse('community')
-    #     else:
-    #         sweetify.sweetalert(request, 'Failed to post.', text='Please ensure the form is valid.', persistent='Ok')
-    # else:
-    #     form = PostForm()
+#     if request.method == 'POST':
+#         form = PostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             post = form.save()
+#             sweetify.sweetalert(request, 'Success! Product Added', timer=1000)
+#             return redirect(reverse('community')
+#         else:
+#             sweetify.sweetalert(request, 'Failed to post.', text='Please ensure the form is valid.', persistent='Ok')
+#     else:
+#         form = PostForm()
 
-    template = 'community/add_post.html'
-    context = {
-        'form': form,
-    }
+#     template = 'community/add_post.html'
+#     context = {
+#         'form': form,
+#     }
 
-    return render(request, template, context)
+#     return render(request, template, context)
