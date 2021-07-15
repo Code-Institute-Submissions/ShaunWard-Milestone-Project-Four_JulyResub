d37ec289ce19f3be1e360c39091cd3a0d49fbb1b
ShaunWard <shaunalwynward@googlemail.com>
2021-07-15 16:26:39 +0000

    Changed author section of dropdown form to textinput

--- community_form/forms.py
+++ community_form/forms.py
@@ -6,3 +6,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'subject', 'content', 'author')
+
+        widgets = {
+            'author': forms.TextInput(attrs={'id': 'author-input', 'class': 'form-control'}),
+        }
