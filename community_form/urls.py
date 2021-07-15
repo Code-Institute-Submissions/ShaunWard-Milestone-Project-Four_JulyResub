ce149c7c6f015fb2de60a6d980868f585bc004c0
ShaunWard <shaunalwynward@googlemail.com>
2021-07-15 13:26:20 +0000

    Added edit form page and urls and view

--- community_form/urls.py
+++ community_form/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CommunityView, PostDetail, AddPostView
+from .views import CommunityView, PostDetail, AddPostView, EditPostView
 
 urlpatterns = [
     path('', CommunityView.as_view(), name='community_form'),
     path('post/<int:pk>', PostDetail.as_view(), name='post_detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('edit_post/<int:pk>', EditPostView.as_view(), name='edit_post'),
 ]
--- community_form/views.py
+++ community_form/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 from .forms import PostForm
 
@@ -18,3 +18,9 @@ class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
+
+
+class EditPostView(UpdateView):
+    model = Post
+    template_name = 'edit_post.html'
+    fields = ['title', 'content']
