8d607d9a40cd76f17ac0ba3f0ca8c4a1a45ced05
ShaunWard <shaunalwynward@googlemail.com>
2021-07-15 13:49:56 +0000

    Added delete post functionality in urls, views and templates

--- community_form/urls.py
+++ community_form/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CommunityView, PostDetail, AddPostView, EditPostView
+from .views import CommunityView, PostDetail, AddPostView, EditPostView, DeletePostView
 
 urlpatterns = [
     path('', CommunityView.as_view(), name='community_form'),
     path('post/<int:pk>', PostDetail.as_view(), name='post_detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('edit_post/<int:pk>', EditPostView.as_view(), name='edit_post'),
+    path('delete_post/<int:pk>', DeletePostView.as_view(), name='delete_post'),
 ]
--- community_form/views.py
+++ community_form/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm
+from django.urls import reverse_lazy
 
 
 class CommunityView(ListView):
     model = Post
     template_name = 'community_form.html'
+    ordering = ['-date_of_post']
 
 
 class PostDetail(DetailView):
@@ -24,3 +26,9 @@ class EditPostView(UpdateView):
     model = Post
     template_name = 'edit_post.html'
     fields = ['title', 'content']
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('community_form')
