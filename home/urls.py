5a38e14136cb7a0e292bc93f921f4b9c802bac1d
ShaunWard <shaunalwynward@googlemail.com>
2021-07-19 21:38:37 +0000

    Resloved pep8 issues in home app

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'), # route url that will render index.html from views.py
+    path('', views.index, name='home'),
 ]
